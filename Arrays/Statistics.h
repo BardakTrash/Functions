d6756adeff09d5d9788341c25224eb8d72fc52bb
BardakTrash <104521716+BardakTrash@users.noreply.github.com>
2022-09-15 17:21:04 +0300

    Seperation DONE

---
+++ Arrays/Statistics.h
@@ -0,0 +1,11 @@
+#pragma once
+
+int Sum(int arr[], const int n);
+double Sum(double arr[], const int n);
+//int Sum(int arr[ROWS][COLS], const int ROLS, const int COLS);
+
+double Avg(int arr[], const int n);
+double Avg(double arr[], const int n);
+
+int minValueIn(int arr[], const int n);
+int maxValueIn(int arr[], const int n);
