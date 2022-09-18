b427eae9c2013ae653e18f0fba9e087d5e226ea6
BardakTrash <104521716+BardakTrash@users.noreply.github.com>
2022-09-18 14:51:28 +0300

    SeparatedTemplates

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,13 +5,6 @@
 #include"Statistics.h"
 #include"Shifts.h"
 
-
-template<typename T>
-void shiftLeft(T arr[], const int n, int number_of_shifts);
-template<typename T>
-void shiftRight(T arr[], const int n, int number_of_shifts);
-
-
 int main()
 {
 	setlocale(LC_ALL, "");
@@ -62,83 +55,4 @@ int main()
 	//maxValueIn(i_arr_2, ROWS, COLS);
 	//Sum(i_arr_2, ROWS, COLS);
 	//Sort(i_arr_2, ROWS, COLS);
-}
-
-template<typename T>
-void Sort(T arr[], const int n)
-{
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = i + 1; j < n; j++)
-		{
-			if (arr[j] < arr[i])
-			{
-				int buffer = arr[i];
-				arr[i] = arr[j];
-				arr[j] = buffer;
-			}
-		}
-	}
-}
-template<typename T>
-int Sum(T arr[], const int n)
-{
-	int sum = 0;
-	for (int i = 0; i < n; i++)
-	{
-		sum += arr[i];
-	}
-	return sum;
-	
-}
-template<typename T>
-double Avg(T arr[], const int n)
-{
-	return (double)Sum(arr, n) / n;
-}
-template<typename T>
-int minValueIn(T arr[], const int n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] < min)min = arr[i];
-	}
-	return min;
-}
-template<typename T>
-int maxValueIn(T arr[], const int n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] > max)max = arr[i];
-	}
-	return max;
-}
-template<typename T>
-void shiftLeft(T arr[], const int n, int number_of_shifts)
-{
-	for (int i = 0; i < number_of_shifts; i++)
-	{
-		int buffer = arr[0];
-		for (int i = 0; i < n; i++)
-		{
-			arr[i] = arr[i + 1];
-		}
-		arr[n - 1] = buffer;
-	}
-}
-template<typename T>
-void shiftRight(T arr[], const int n, int number_of_shifts)
-{
-	for (int i = 0; i < number_of_shifts; i++)
-	{
-		int buffer = arr[n - 1];
-		for (int i = n - 1; i > 0; i--)
-		{
-			arr[i] = arr[i - 1];
-		}
-		arr[0] = buffer;
-	}
 }
