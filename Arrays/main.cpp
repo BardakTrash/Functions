7dc4354b1c351ab70d676f41365ea082afc95349
BardakTrash <104521716+BardakTrash@users.noreply.github.com>
2022-09-18 14:42:08 +0300

    SeparatedTemplates

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -5,24 +5,9 @@
 #include"Statistics.h"
 #include"Shifts.h"
 
-template<typename T>
-void Sort(T arr[], const int n);
-
-template<typename T>
-int Sum(T arr[], const int n);
-
-template<typename T>
-double Avg(T arr[], const int n);
-
-template<typename T>
-int minValueIn(T arr[], const int n);
-
-template<typename T>
-int maxValueIn(T arr[], const int n);
 
 template<typename T>
 void shiftLeft(T arr[], const int n, int number_of_shifts);
-
 template<typename T>
 void shiftRight(T arr[], const int n, int number_of_shifts);
 
